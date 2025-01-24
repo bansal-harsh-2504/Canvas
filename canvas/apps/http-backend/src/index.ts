822d1f60c5b26896a49af83acb216a1ea0a7b704
harsh <focusedstudytarget2022@gmail.com>
2025-01-24 23:08:29 +0530

    basic backend

--- canvas/apps/http-backend/src/index.ts
+++ canvas/apps/http-backend/src/index.ts
@@ -1,7 +1,41 @@
 import express from "express";
+import "dotenv/config";
+import jwt from "jsonwebtoken";
+import { middleware } from "./middleware";
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server listening to port 3000");
+app.post("/signup", (req, res) => {
+  res.json({
+    userID: 123,
+  });
+});
+
+app.post("/signin", (req, res) => {
+  const userId = 1;
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    process.env.JWT_SECRET as string
+  );
+
+  res.json({
+    token,
+  });
+});
+
+app.post("/room", middleware, (req, res) => {
+  res.json({
+    roomID: 123,
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("API Working!");
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is listening to port ${PORT}`);
 });
