83e2e0ee8b6c1fc7217bfeaf85a6c1c58a052bd5
harsh <focusedstudytarget2022@gmail.com>
2025-01-26 00:08:17 +0530

    added common zod schema

--- canvas/apps/http-backend/src/index.ts
+++ canvas/apps/http-backend/src/index.ts
@@ -3,16 +3,39 @@ import "dotenv/config";
 import jwt from "jsonwebtoken";
 import { middleware } from "./middleware/auth.middleware";
 import { JWT_SECRET } from "@repo/backend-common/config";
+import {
+  createUserSchema,
+  signInSchema,
+  createRoomSchema,
+} from "@repo/common/types";
 
 const app = express();
 
 app.post("/signup", (req, res) => {
+  const data = createUserSchema.safeParse(req.body);
+
+  if (!data.success) {
+    res.json({
+      message: "Incorrect inputs",
+    });
+    return;
+  }
+
   res.json({
     userID: 123,
   });
 });
 
 app.post("/signin", (req, res) => {
+  const data = signInSchema.safeParse(req.body);
+
+  if (!data.success) {
+    res.json({
+      message: "Incorrect inputs",
+    });
+    return;
+  }
+
   const userId = 1;
   const token = jwt.sign(
     {
@@ -27,6 +50,15 @@ app.post("/signin", (req, res) => {
 });
 
 app.post("/room", middleware, (req, res) => {
+  const data = createRoomSchema.safeParse(req.body);
+
+  if (!data.success) {
+    res.json({
+      message: "Incorrect inputs",
+    });
+    return;
+  }
+
   res.json({
     roomID: 123,
   });
