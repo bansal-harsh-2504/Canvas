c6932807afc67c5b5a28fd008d8dabf947c24fb6
harsh <focusedstudytarget2022@gmail.com>
2025-01-31 06:33:14 +0530

    added react-hot-toast

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { Navbar } from "../components/Navbar";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Navbar />
         {children}
+        <Toaster position="bottom-right" />
       </body>
     </html>
   );
